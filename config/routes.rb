7de3ac1b6626d3127e2016c8894090577ac1a7d3
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-26 16:00:05 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
-
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-
-  root 'posts#index'
-
+resources :authors, only: [:show] do
+    # nested resource for posts
+   resources :posts, only: [:show, :index]
+   root 'posts#index'
+  end  
+    resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 end
