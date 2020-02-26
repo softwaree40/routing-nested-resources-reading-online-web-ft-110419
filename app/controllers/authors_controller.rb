aebc19da70a6cda8755d24c22edbfe7b7410a192
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-26 17:47:31 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,36 @@
 class AuthorsController < ApplicationController
 
+  def index
+    @authors = Author.all
+  end
+
   def show
     @author = Author.find(params[:id])
   end
 
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+    @author.save
+    redirect_to author_path(@author)
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    @author.update(author_params)
+    redirect_to author_path(@author)
+  end
+
+  def edit
+    @author = .find(params[:id])
+  end
+
+private
+
+  def author_params
+    params.require(:author).permit(:name)
+  end
 end
